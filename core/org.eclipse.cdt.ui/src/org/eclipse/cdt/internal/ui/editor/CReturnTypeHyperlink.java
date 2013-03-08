65e819c4d3af2000aa544a1eef57aaecc62b4d54
Marc-Andre Laperle <malaperle@gmail.com>
2013-03-08 10:33:34 -0500

    More work?

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/CReturnTypeHyperlink.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/CReturnTypeHyperlink.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Marc-Andre Laperle and others.
+ * Copyright (c) 2011, 2012 Marc-Andre Laperle and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,6 +20,7 @@ import org.eclipse.cdt.ui.CUIPlugin;
 
 import org.eclipse.cdt.internal.ui.viewsupport.EditorOpener;
 
+//TODO: extend CElementHyperlink
 public class CReturnTypeHyperlink implements IHyperlink {
 
 	private final IRegion region;
--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/CReturnTypeHyperlinkDetector.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/CReturnTypeHyperlinkDetector.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2011 Marc-Andre Laperle and others.
+ * Copyright (c) 2011, 2012 Marc-Andre Laperle and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * Marc-Andre Laperle - Initial API and implementation
+ *     Marc-Andre Laperle - Initial API and implementation
  *******************************************************************************/
 package org.eclipse.cdt.internal.ui.editor;
 
@@ -18,23 +18,18 @@ import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 import org.eclipse.cdt.core.dom.ast.IASTName;
-import org.eclipse.cdt.core.dom.ast.IArrayType;
 import org.eclipse.cdt.core.dom.ast.IBasicType;
 import org.eclipse.cdt.core.dom.ast.IBinding;
 import org.eclipse.cdt.core.dom.ast.ICompositeType;
 import org.eclipse.cdt.core.dom.ast.IFunction;
-import org.eclipse.cdt.core.dom.ast.IPointerType;
-import org.eclipse.cdt.core.dom.ast.IQualifierType;
 import org.eclipse.cdt.core.dom.ast.IType;
-import org.eclipse.cdt.core.dom.ast.ITypedef;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPFunction;
-import org.eclipse.cdt.core.dom.ast.cpp.ICPPReferenceType;
-import org.eclipse.cdt.core.dom.ast.cpp.ICPPSpecialization;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPTemplateInstance;
 import org.eclipse.cdt.core.index.IIndex;
 import org.eclipse.cdt.core.model.IWorkingCopy;
 import org.eclipse.cdt.ui.CUIPlugin;
 
+import org.eclipse.cdt.internal.core.dom.parser.cpp.semantics.SemanticUtil;
 import org.eclipse.cdt.internal.core.model.ext.ICElementHandle;
 
 import org.eclipse.cdt.internal.ui.viewsupport.IndexUI;
@@ -49,48 +44,37 @@ public class CReturnTypeHyperlinkDetector extends CElementHyperlinkDetector {
 				IBinding binding = selectedName.resolveBinding();
 				if (binding instanceof IFunction) {
 					IType returnType = ((ICPPFunction) binding).getType().getReturnType();
-
-					returnType = getStrippedType(returnType);
-					if(returnType instanceof IBasicType) {
+					returnType = SemanticUtil.getUltimateType(returnType, false);
+					
+					if(returnType instanceof IBasicType || !(returnType instanceof IBinding)) {
 						return null;
 					}
 					
-					IBinding finalReturnType = null;
-					if(returnType instanceof IBinding) {
-						finalReturnType = (IBinding) returnType;
-					}
+					IBinding returnTypeBinding = (IBinding) returnType;
 					
-					while(returnType instanceof IQualifierType) {
-						returnType = ((IQualifierType) returnType).getType();
-					}
-					
+					// TODO: steal from somewhere else? Can it be chained?
+					// class, struct, union... could be templated
 					if (returnType instanceof ICompositeType) {
-						ICompositeType iCompositeType = (ICompositeType) returnType;
-						finalReturnType = iCompositeType;
+						returnTypeBinding = (ICompositeType) returnType;
 
 						if (returnType instanceof ICPPTemplateInstance) {
 							ICPPTemplateInstance icppTemplateInstance = (ICPPTemplateInstance) returnType;
-							finalReturnType = icppTemplateInstance.getTemplateDefinition();
+							returnTypeBinding = icppTemplateInstance.getTemplateDefinition();
 
 						}
 					}
-					
-					if(returnType instanceof ITypedef) {
-						finalReturnType = (ITypedef) returnType;
-						if(finalReturnType instanceof ICPPSpecialization) {
-							ICPPSpecialization icppSpecialization = (ICPPSpecialization) finalReturnType;
-							finalReturnType = icppSpecialization.getSpecializedBinding();
-						}
-					}
-					
-					if(finalReturnType != null) {
 
-						ICElementHandle[] foundElements = IndexUI.findAllDefinitions(index, finalReturnType);
-
-						if (foundElements.length == 0) {
-							foundElements = new ICElementHandle[1];
-							foundElements[0] = IndexUI.findAnyDeclaration(index, null, finalReturnType);
-						}
+					// TODO: need this??
+//					if(returnType instanceof ITypedef) {
+//						returnTypeBinding = (ITypedef) returnType;
+//						if(finalReturnType instanceof ICPPSpecialization) {
+//							ICPPSpecialization icppSpecialization = (ICPPSpecialization) finalReturnType;
+//							returnTypeBinding = icppSpecialization.getSpecializedBinding();
+//						}
+//					}
+					
+					if(returnTypeBinding != null) {
+						ICElementHandle[] foundElements = IndexUI.findRepresentative(index, returnTypeBinding);						
 						if (foundElements.length > 0 && foundElements[0] != null) {
 							ITextEditor textEditor = (ITextEditor) getAdapter(ITextEditor.class);
 							if (textEditor != null) {
@@ -108,26 +92,6 @@ public class CReturnTypeHyperlinkDetector extends CElementHyperlinkDetector {
 			}
 		}
 		return null;
-	}
-
-	/**
-	 * @param returnType
-	 */
-	protected IType getStrippedType(IType returnType) {
-		while (returnType instanceof IPointerType || returnType instanceof ICPPReferenceType || returnType instanceof IArrayType) {
-			if (returnType instanceof IPointerType) {
-				IPointerType iPointerType = (IPointerType) returnType;
-				returnType = iPointerType.getType();
-			} else if (returnType instanceof ICPPReferenceType) {
-				ICPPReferenceType icppReferenceType = (ICPPReferenceType) returnType;
-				returnType = icppReferenceType.getType();
-			} else if (returnType instanceof IArrayType) {
-				IArrayType iArrayType = (IArrayType) returnType;
-				returnType = iArrayType.getType();
-			}
-		}
-
-		return returnType;
 	}
 
 	@Override
